7f7a8ae42b9056fbeeec8ccf71a834dab70f8042
SomGitHub2020 <61223781+SomGitHub2020@users.noreply.github.com>
2020-07-10 18:13:59 +0530

    Initial commit

---
+++ todo/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework import viewsets          
+from .serializers import TodoSerializer     
+from .models import Todo        
+from django_filters.rest_framework import DjangoFilterBackend      
+
+# Create your views here.
+
+
+
+class TodoView(viewsets.ModelViewSet):       
+    serializer_class = TodoSerializer        
+    queryset = Todo.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'description']
